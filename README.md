31dc6a2eafa668a90cafddbf91ba5ce4c8132a63
Carlos Ferreras <carlos@carlosferreras.com>
2021-01-04 21:34:56 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Cryptocurrency Price Status Discord Bot
+
+This bot will update its status message with the current price of the desired cryptocurrency.
+Is based on [Minecraft Player Count Discord Bot](https://github.com/SpencerTorres/Minecraft-Player-Count-Discord-Bot) by Spencer Torres
+
+![Example bot setup.](https://i.imgur.com/AZo7Z9H.png)
+
+## How to use
+
+This bot is really easy to use.
+
+Just follow these steps:
+1. Have [Node.JS](https://nodejs.org) installed.
+2. Clone this repository to a folder on your computer.
+3. Open a terminal in that folder, and install the packages with `npm install`
+4. Open the `.env-template` file and configure it to your liking.
+
+For information on getting a bot token, follow the steps on [the Discord developer documentation.](https://discordapp.com/developers/docs/intro)
+
+## Extra Info
+
+This was created for my Discord server, but I wanted to share it with added flexibility for anyone to use.
+
+This relies on the API hosted at https://api.coingecko.com
---
+++ index.js
@@ -0,0 +1,44 @@
+require('dotenv').config() // Load .env file
+const axios = require('axios')
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+function getPrices() {
+
+
+	// API for price data.
+	axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${process.env.PREFERRED_CURRENCY}&ids=${process.env.COIN_ID}`).then(res => {
+		// If we got a valid response
+		if(res.data && res.data[0].current_price && res.data[0].price_change_percentage_24h) {
+			let currentPrice = res.data[0].current_price || 0 // Default to zero
+			let priceChange = res.data[0].price_change_percentage_24h || 0 // Default to zero
+			let symbol = res.data[0].symbol || '?' 
+			client.user.setPresence({
+				game: {
+					// Example: "Watching -5,52% | BTC"
+					name: `${priceChange.toFixed(2)}% | ${symbol.toUpperCase()}`,
+					type: 3 // Use activity type 3 which is "Watching"
+				}
+			})
+
+			client.guilds.find(guild => guild.id === process.env.SERVER_ID).me.setNickname(`${(currentPrice).toLocaleString().replace(/,/g,process.env.THOUSAND_SEPARATOR)}${process.env.CURRENCY_SYMBOL}`)
+
+			console.log('Updated price to', currentPrice)
+		}
+		else
+			console.log('Could not load player count data for', process.env.COIN_ID)
+
+	}).catch(err => console.log('Error at api.coingecko.com data:', err))
+}
+
+// Runs when client connects to Discord.
+client.on('ready', () => {
+	console.log('Logged in as', client.user.tag)
+
+	getPrices() // Ping server once on startup
+	// Ping the server and set the new status message every x minutes. (Minimum of 1 minute)
+	setInterval(getPrices, Math.max(1, process.env.MC_PING_FREQUENCY || 1) * 60 * 1000)
+})
+
+// Login to Discord
+client.login(process.env.DISCORD_TOKEN)
